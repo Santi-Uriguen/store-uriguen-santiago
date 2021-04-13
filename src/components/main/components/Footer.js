de33f32d3b8a768a752b928404d0c7119366c1ab
SantiUriguen <santiuriguen@hotmail.com>
2021-04-13 12:15:16 -0300

    final styles (2)

--- src/components/main/components/Footer.js
+++ src/components/main/components/Footer.js
@@ -2,8 +2,11 @@ import PageButton from "./PageButton.js";
 export default function Footer() {
   return (
     <section className="footer" id="footerContainer">
-      <div id="counterDown">16 of 32 products</div>
-      <PageButton />
+      <div className="footerInfo">
+        <div id="counterDown">16 of 32 products</div>
+        <PageButton />
+      </div>
+      <span></span>
     </section>
   );
 }
